5790ddb9a4cd74caf086264307627a2efe71e10a
JOHN DAVID C. AJON <jdcajon@mymail.mapua.edu.ph>
2026-08-04 11:38:48 +0800

    Updated test_main.py and Screenshot labels

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,13 @@
 from src.main import greet
 
-def test_greet():
+
+def test_greet_john():
     assert greet("John") == "Hello, John!"
+
+
+def test_greet_maria():
     assert greet("Maria") == "Hello, Maria!"
+
+
+def test_greet_cpe106l():
     assert greet("CPE106L") == "Hello, CPE106L!"
